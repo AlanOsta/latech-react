bed6758597edcb413801a3265f9cf9ca5746c763
Alan Osta <alanosta@gmail.com>
2021-03-03 12:46:43 -0300

    Retoques finales

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 import calendarioReducer from './api/calendarioReducer';
 import thunk from "redux-thunk"
+//import { compose } from 'redux';
 
-//const store = createStore(calendarioReducer,compose (applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+//const store = createStore( calendarioReducer, compose( applyMiddleware(thunk), window.navigator.userAgent.includes('Chrome') ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose, ), )
 
-const store = createStore( calendarioReducer, compose( applyMiddleware(thunk), window.navigator.userAgent.includes('Chrome') ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose, ), )
+const store = createStore( calendarioReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
